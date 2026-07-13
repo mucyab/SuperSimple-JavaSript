6b3a40f7b6c689ac5cba341562304ddadb6d73f4
MUCYAB <musiyyab@gmail.com>
2026-07-13 12:24:07 +0500

    Initialize todoList with default items and render HTML output

--- Todo List/11-Todo-List.js
+++ Todo List/11-Todo-List.js
@@ -1,4 +1,15 @@
-const todoList = [];
+const todoList = ['make dinner', 'wash dishes'];
+
+let todoListHtml = ' ';
+
+
+for (let i = 0; i < todoList.length ;  i++) {
+    const todo = todoList[i];
+    const html = `<p>${todo}</p>`;
+    todoListHtml += html; 
+} 
+console.log(todoListHtml);
+
 
 function addTodo() {
     const inputElement = document.querySelector('.js-name-input');
