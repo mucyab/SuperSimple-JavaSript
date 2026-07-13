ee2090a7bcfd6471c9844c317c9844f7f90e20e4
MUCYAB <musiyyab@gmail.com>
2026-07-13 12:30:16 +0500

    Refactor todo list rendering and improve code consistency

--- Todo List/11-Todo-List.js
+++ Todo List/11-Todo-List.js
@@ -1,23 +1,27 @@
-const todoList = ['make dinner', 'wash dishes'];
+const todoList = ["make dinner", "wash dishes"];
 
-let todoListHtml = ' ';
+renderTodoList ();
+function renderTodoList() {
+  let todoListHtml = " ";
 
-
-for (let i = 0; i < todoList.length ;  i++) {
+  for (let i = 0; i < todoList.length; i++) {
     const todo = todoList[i];
     const html = `<p>${todo}</p>`;
-    todoListHtml += html; 
-} 
-console.log(todoListHtml);
+    todoListHtml += html;
+  }
+  console.log(todoListHtml);
 
+  document.querySelector(".js-todo-list").innerHTML = todoListHtml;
+}
 
 function addTodo() {
-    const inputElement = document.querySelector('.js-name-input');
-    const name = inputElement.value;
-    
+  const inputElement = document.querySelector(".js-name-input");
+  const name = inputElement.value;
 
-    todoList.push(name);
-    console.log(todoList);
-    
-    inputElement.value = '';
+  todoList.push(name);
+  console.log(todoList);
+
+  inputElement.value = "";
+
+  renderTodoList ();
 }
