95964c76ec643e3ee5e8f44b6b4e5e2910a9c5db
MUCYAB <musiyyab@gmail.com>
2026-08-05 11:40:24 +0500

    autoPlay done in Rock Paper Scissors Game

--- 12-Rock Paper Scissor/12-rock-paper-scissors.js
+++ 12-Rock Paper Scissor/12-rock-paper-scissors.js
@@ -1,7 +1,7 @@
-let score = JSON.parse(localStorage.getItem('score')) || {
+let score = JSON.parse(localStorage.getItem("score")) || {
   wins: 0,
   losses: 0,
-  ties: 0
+  ties: 0,
 };
 
 updateScoreElement();
@@ -16,89 +16,87 @@ if (!score) {
 }
 */
 let isAutoPlaying = false;
-
+let intervalId;
 function autoPlay() {
   if (!isAutoPlaying) {
-    setInterval(function (){
+    intervalId = setInterval(function () {
       const playerMove = pickComputerMove();
       playGame(playerMove);
     }, 1000);
     isAutoPlaying = true;
   } else {
-    
+    clearInterval(intervalId);
+    isAutoPlaying = false;
   }
 }
-
 
 function playGame(playerMove) {
   const computerMove = pickComputerMove();
 
-  let result = '';
+  let result = "";
 
-  if (playerMove === 'scissors') {
-    if (computerMove === 'rock') {
-      result = 'You lose.';
-    } else if (computerMove === 'paper') {
-      result = 'You win.';
-    } else if (computerMove === 'scissors') {
-      result = 'Tie.';
+  if (playerMove === "scissors") {
+    if (computerMove === "rock") {
+      result = "You lose.";
+    } else if (computerMove === "paper") {
+      result = "You win.";
+    } else if (computerMove === "scissors") {
+      result = "Tie.";
     }
-
-  } else if (playerMove === 'paper') {
-    if (computerMove === 'rock') {
-      result = 'You win.';
-    } else if (computerMove === 'paper') {
-      result = 'Tie.';
-    } else if (computerMove === 'scissors') {
-      result = 'You lose.';
+  } else if (playerMove === "paper") {
+    if (computerMove === "rock") {
+      result = "You win.";
+    } else if (computerMove === "paper") {
+      result = "Tie.";
+    } else if (computerMove === "scissors") {
+      result = "You lose.";
     }
-    
-  } else if (playerMove === 'rock') {
-    if (computerMove === 'rock') {
-      result = 'Tie.';
-    } else if (computerMove === 'paper') {
-      result = 'You lose.';
-    } else if (computerMove === 'scissors') {
-      result = 'You win.';
+  } else if (playerMove === "rock") {
+    if (computerMove === "rock") {
+      result = "Tie.";
+    } else if (computerMove === "paper") {
+      result = "You lose.";
+    } else if (computerMove === "scissors") {
+      result = "You win.";
     }
   }
 
-  if (result === 'You win.') {
+  if (result === "You win.") {
     score.wins += 1;
-  } else if (result === 'You lose.') {
+  } else if (result === "You lose.") {
     score.losses += 1;
-  } else if (result === 'Tie.') {
+  } else if (result === "Tie.") {
     score.ties += 1;
   }
 
-  localStorage.setItem('score', JSON.stringify(score));
+  localStorage.setItem("score", JSON.stringify(score));
 
   updateScoreElement();
 
-  document.querySelector('.js-result').innerHTML = result;
+  document.querySelector(".js-result").innerHTML = result;
 
-  document.querySelector('.js-moves').innerHTML = `You
+  document.querySelector(".js-moves").innerHTML = `You
 <img src="images/${playerMove}-emoji.png" class="move-icon">
 <img src="images/${computerMove}-emoji.png" class="move-icon">
 Computer`;
 }
 
 function updateScoreElement() {
-  document.querySelector('.js-score')
-    .innerHTML = `Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}`;
+  document.querySelector(".js-score").innerHTML =
+    `Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}`;
 }
 
 function pickComputerMove() {
   const randomNumber = Math.random();
 
-  let computerMove = '';
+  let computerMove = "";
 
   if (randomNumber >= 0 && randomNumber < 1 / 3) {
-    computerMove = 'rock';
+    computerMove = "rock";
   } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
-    computerMove = 'paper';
+    computerMove = "paper";
   } else if (randomNumber >= 2 / 3 && randomNumber < 1) {
-    computerMove = 'scissors';
+    computerMove = "scissors";
   }
 
   return computerMove;
