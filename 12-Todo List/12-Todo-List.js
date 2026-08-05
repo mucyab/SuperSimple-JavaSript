bc5184d037614a08319032f0afee07c6c5aa9e4b
MUCYAB <musiyyab@gmail.com>
2026-08-05 11:46:55 +0500

    adding .forEach() on todo list

---
+++ 12-Todo List/12-Todo-List.js
@@ -0,0 +1,53 @@
+const todoList = [
+  {
+    name: "make dinner",
+    dueDate: "2022-12-23",
+  },
+  {
+    name: "wash dishies",
+    dueDate: "2022-12-23",
+  },
+];
+
+renderTodoList();
+function renderTodoList() {
+  let todoListHtml = "";
+  // Generate HTML for user automatically,
+  for (let i = 0; i < todoList.length; i++) {
+    const todoObject = todoList[i];
+    const { name, dueDate } = todoObject;
+
+    const html = `
+    <div class="todo-grid">
+      <div>${name}</div> 
+      <div>${dueDate}</div>
+      <button onclick="
+        todoList.splice(${i}, 1);
+        renderTodoList();
+        " class= "delete-todo-button"> Delete </button> 
+      </div>`;
+    todoListHtml += html;
+  }
+
+  document.querySelector(".js-todo-list").innerHTML = todoListHtml;
+}
+// Main Idea of JavaScript. Creating websites with JavaScript we follow three step process, 1. save the data, 2. Generate the HTML, 3. Make website interactive.
+
+function addTodo() {
+  const inputElement = document.querySelector(".js-name-input");
+  const name = inputElement.value;
+
+  const dateElement = document.querySelector(".js-due-date-input");
+  const dueDate = dateElement.value;
+
+  todoList.push({
+    // name: name,
+    // dueDate: dueDate,
+    name,
+    dueDate,
+  });
+
+  inputElement.value = "";
+
+  renderTodoList();
+}
